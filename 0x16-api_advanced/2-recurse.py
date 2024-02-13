6f8f4d91dfe616c3dc1dbfe020ca01c6fdf30f82
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2024-02-13 14:11:15 +0000

    tasks

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,7 +7,7 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "xica369"
     }
     params = {
         "after": after,
@@ -17,7 +17,7 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 404:
-        return None
+        return (None)
 
     results = response.json().get("data")
     after = results.get("after")
@@ -26,5 +26,5 @@ def recurse(subreddit, hot_list=[], after="", count=0):
         hot_list.append(c.get("data").get("title"))
 
     if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+        return (recurse(subreddit, hot_list, after, count))
+    return (hot_list)
