d6c1a2b6c1679239c2d4e5d726dd43c759b38a12
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2024-02-14 07:59:50 +0000

    t0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,6 +9,6 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     res = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 404:
+    if res.status_code == 404:
         return (0)
-    return (response.json().get("data").get("subscribers"))
+    return (res.json().get("data").get("subscribers"))
